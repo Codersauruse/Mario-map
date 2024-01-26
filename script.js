c140253130b824d1567b40af08c7943106fb1811
Auother <104962511+Codersauruse@users.noreply.github.com>
2024-01-26 10:53:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function initMap() {
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 6.9497,
+            lng: 80.7891
+        },
+        zoom: 8.8,
+        mapId: '8b5581dbaab433b8'
+    });
+
+}
